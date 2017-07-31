35b5645e861850eab83c26e4fa9992ff10dbc3cb
Aleme Gabriel <egaleme@gmail.com>
2017-07-31 22:29:47 +0100

    updated to use without email verification

--- lib/stockitServer/web/auth.ex
+++ lib/stockitServer/web/auth.ex
@@ -6,11 +6,11 @@ defmodule StockitServer.Web.Auth do
 		user = Account.get_by(username)
 		if user do
 			cond do
-			user.email_verified && check_password_hash(given_pass, user.password_hash) ->
+			user && check_password_hash(given_pass, user.password_hash) ->
 				{:ok, user}
-			user.email_verified && check_password_hash(given_pass, user.password_hash) == false ->
+			user && check_password_hash(given_pass, user.password_hash) == false ->
 				{:err, :unauthorized}
-			user.email_verified == false ->
+			not user ->
 				{:err, :notverified}
 			end
 		else
