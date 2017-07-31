e07da0a17e76ee190d771f5200ab028ea466e77e
Aleme Gabriel <egaleme@gmail.com>
2017-07-31 07:49:49 +0100

    updated to use heroku as my hosting

--- lib/stockitServer/user_email.ex
+++ lib/stockitServer/user_email.ex
@@ -6,6 +6,6 @@ defmodule StockitServer.UserEmail do
 		|> to({user.name, user.email})
 		|> from({"stockdiary", "stockdiaryapp@gmail.com"})
 		|> subject("Your stockIT verification email")
-		|> html_body("<h1>stockIT</h1><p>Thanks for signing up with us</p><p>Please click the link below to verify your email address</p><a href=http://localhost:4000/v1/api/auth/verify_email/#{user.token}>Verify address</a>")
+		|> html_body("<h1>stockIT</h1><p>Thanks for signing up with us</p><p>Please click the link below to verify your email address</p><a href=https://stockitserver.herokuapp.com/v1/api/auth/verify_email/#{user.token}>Verify address</a>")
 	end
 end
